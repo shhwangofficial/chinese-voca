c6bb34e6dcc4f97568b1155d93924580cd82c066
shhwangofficial <shhwangofficial@gmail.com>
2025-04-15 14:50:19 +0900

    Set main page

--- newpjt/urls.py
+++ newpjt/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('words/', include('words.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='words/')),
 ]
