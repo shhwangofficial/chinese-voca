6b585cacf5aa76e212359c86be23dc26c4a7969a
shhwangofficial <shhwangofficial@gmail.com>
2025-04-23 15:50:47 +0900

    singup and some decoraters

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST
 
+@require_http_methods(['GET', 'POST'])
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('words:index')
     if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -16,6 +22,24 @@ def login(request):
     }
     return render(request, 'accounts/login.html', context)
 
+@login_required
+@require_POST
 def logout(request):
     auth_logout(request)
-    return redirect('words:index') 
+    return redirect('words:index')
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('words:index')
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('words:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'accounts/signup.html', context)
